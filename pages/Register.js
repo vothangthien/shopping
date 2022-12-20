41b5e0abece1f85e55129333472de7cb74763336
vo son <son018101046@tgu.edu.vn>
2022-12-20 11:49:08 -0800

    'updata'

--- pages/Register.js
+++ pages/Register.js
@@ -1,36 +1,58 @@
 import React from 'react'
 import Link from 'next/link'
+import { useState,useContext } from 'react'
+import valid from '../utils/valid';
+
+
 
 function Register() {
+const initialState={name:'',email:'',password:'',cf_password:''};
+const [userData,setUserData]=useState(initialState);
+const {name,email,password,cf_password}=userData;
+
+const handlechangeinput=e=>{
+  const{name,value}=e.target;
+  setUserData({...userData,[name]:value});
+}
+
+const handleSubmit=e=>{
+e.preventDefault();
+const errMsg=valid(name,email,password,cf_password);
+if(errMsg) console.log(errMsg);
+
+};
+
+
+
   return (
     <div>
-    <form className='mx-auto my-4' style={{maxWidth:'500px' }}>
-    <div class="mb-3">
-         <label for="name" class="form-label">NAME</label>
-         <input type="email" class="form-control" id="name" 
-           name='name'></input>
+    <form className='mx-auto my-4' style={{maxWidth:'500px' }} onSubmit={handleSubmit} >
+    <div className="mb-3">
+         <label forhtml="name" className="form-label">NAME</label>
+         <input type="text" className="form-control" id="name" 
+           name='name'value={name} onChange={handlechangeinput}></input>
        </div>
 
-       <div class="mb-3">
-         <label for="exampleInputEmail1" class="form-label">Email address</label>
-         <input type="email" class="form-control" id="exampleInputEmail1" 
-           name='email'></input>
+       <div className="mb-3">
+         <label forhtml="exampleInputEmail1" className="form-label">Email address</label>
+         <input type="email" className="form-control" id="exampleInputEmail1" 
+           name='email'value={email} onChange={handlechangeinput}></input>
        </div>
 
 
-    <div class="mb-3">
-     <label for="exampleInputPassword1" class="form-label">Password</label>
-     <input type="password" class="form-control" id="exampleInputPassword1"
-       name='password'></input>
+    <div className="mb-3">
+     <label forhtml="exampleInputPassword1" className="form-label">Password</label>
+     <input type="password" className="form-control" id="exampleInputPassword1"
+       name='password'value={password} onChange={handlechangeinput}></input>
    </div>
 
-   <div class="mb-3">
-     <label for="exampleInputPassword1" class="form-label">comfrim Password</label>
-     <input type="password" class="form-control" id="exampleInputPassword2"
-       name='cf_password'></input>
+   <div className="mb-3">
+     <label forhtml="exampleInputPassword1" className="form-label">comfrim Password</label>
+     <input type="password" className="form-control" id="exampleInputPassword2"
+       name='cf_password' value={cf_password} onChange={handlechangeinput}></input>
    </div>
 
-    <button type="submit" class="btn btn-primary">REGISTER</button>
+    <button type="submit" className="btn btn-primary">REGISTER</button>
     <p className='my-2' >
       Already have an account? <Link href="/Signin" legacyBehavior><a style={{color: 'crimson'}}>Login Now</a></Link>
     </p>
--- pages/Signin.js
+++ pages/Signin.js
@@ -1,23 +1,44 @@
 import React from 'react'
 import Link from 'next/link'
+import valid from '../utils/valid'
+import { useState,useContext } from 'react'
+
 function Signin() {
+const initialState={email:'',password:'',}
+const [userData,setUserData]=useState(initialState);
+const {email,password}=userData;
+
+const handlechangeinput=e=>{
+ const {name,value}=e.target;
+ setUserData({...userData, [name]:value})
+
+
+}
+
+const handleSubmit=e=>{
+  e.preventDefault()
+console.log(userData)
+  
+}
+
+
   return (
     <div>
-        <form className='mx-auto my-4' style={{maxWidth:'500px' }}>
-           <div class="mb-3">
-             <label for="exampleInputEmail1" class="form-label">Email address</label>
-             <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
-               name='email'></input>
+        <form className='mx-auto my-4' style={{maxWidth:'500px' }} onSubmit={handleSubmit}>
+           <div className="mb-3">
+             <label forhtml="exampleInputEmail1" className="form-label">Email address</label>
+             <input type="email" className="form-control" id="exampleInputEmail1" 
+                  name='email'value={email} onChange={handlechangeinput}></input>
            </div>
 
 
-        <div class="mb-3">
-         <label for="exampleInputPassword1" class="form-label">Password</label>
-         <input type="password" class="form-control" id="exampleInputPassword1"
-           name='password'></input>
+        <div className="mb-3">
+         <label forhtml="exampleInputPassword1" className="form-label">Password</label>
+         <input type="password" className="form-control" id="exampleInputPassword1"
+           name='password'value={password} onChange={handlechangeinput}></input>
        </div>
  
-        <button type="submit" class="btn btn-primary">SIGNIN</button>
+        <button type="submit" className="btn btn-primary">SIGNIN</button>
         <p className='my-2' >
         You not have an account? <Link href="/Register" legacyBehavior><a style={{color: 'crimson'}}>Register Now</a></Link>
         </p>
